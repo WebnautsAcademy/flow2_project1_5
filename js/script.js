ad663ef1b54c04ee07bc0e26930f633c5e8efa88
Andy <adrianrazor@ukr.net>
2021-10-09 10:16:14 +0300

    Project-completed
    Completed the markup and styling of the pages; Added buttons and modal windows

--- js/script.js
+++ js/script.js
@@ -6,24 +6,28 @@ var popupContainerHidden = document.querySelector('.popup-container-hidden')
 var popupCallbackHidden = document.querySelector('.popup-callback-hidden')
 
 // Открывает окно Перезвон
-buttonCall.addEventListener(
-    'click',
-    function () {
-        popupContainerHidden.classList.remove('popup-container-hidden')
-        popupCallbackHidden.classList.remove('popup-callback-hidden')
-    }
-)
+if (buttonCall) {
+    buttonCall.addEventListener(
+        'click',
+        function () {
+            popupContainerHidden.classList.remove('popup-container-hidden')
+            popupCallbackHidden.classList.remove('popup-callback-hidden')
+        }
+    )
+}
 
 // Кнопка закрытия модального окна Перезвон
 var popupCallBtnClose = document.querySelector('.popup-call-btn-close')
 
-popupCallBtnClose.addEventListener(
-    'click',
-    function () {
-        popupContainerHidden.classList.add('popup-container-hidden')
-        popupCallbackHidden.classList.add('popup-callback-hidden')
-    }
-)
+if (popupCallBtnClose) {
+    popupCallBtnClose.addEventListener(
+        'click',
+        function () {
+            popupContainerHidden.classList.add('popup-container-hidden')
+            popupCallbackHidden.classList.add('popup-callback-hidden')
+        }
+    )
+}
 
 
 // Кнопка вызова окна Калькулятор
@@ -31,61 +35,71 @@ var welcomeButton = document.querySelector('.welcome-button')
 // Класс скрытия модального окна Калькулятор
 var popupCalculatorHidden = document.querySelector('.popup-calculator-hidden')
 
-welcomeButton.addEventListener(
-    'click',
-    function () {
-        popupContainerHidden.classList.remove('popup-container-hidden')
-        popupCalculatorHidden.classList.remove('popup-calculator-hidden')
-    }
-)
+if (welcomeButton) {
+    welcomeButton.addEventListener(
+        'click',
+        function () {
+            popupContainerHidden.classList.remove('popup-container-hidden')
+            popupCalculatorHidden.classList.remove('popup-calculator-hidden')
+        }
+    )
+}
 
 // Кнопка закрытия окна Калькулятор
 var popupCalcBtnClose = document.querySelector('.popup-calc-btn-close')
 
-popupCalcBtnClose.addEventListener(
-    'click',
-    function () {
-        popupContainerHidden.classList.add('popup-container-hidden')
-        popupCalculatorHidden.classList.add('popup-calculator-hidden')
-    }
-)
+if (popupCalcBtnClose) {
+    popupCalcBtnClose.addEventListener(
+        'click',
+        function () {
+            popupContainerHidden.classList.add('popup-container-hidden')
+            popupCalculatorHidden.classList.add('popup-calculator-hidden')
+        }
+    )
+}
 
 // Кнопка перехода на окно Вопросы
 var calculatorButton = document.querySelector('.calculator-button')
 // Класс скрытия окна Вопросы
 var popupQuestionsHidden = document.querySelector('.popup-questions-hidden')
 
-calculatorButton.addEventListener(
-    'click',
-    function () {
-        popupCalculatorHidden.classList.add('popup-calculator-hidden')
-        popupQuestionsHidden.classList.remove('popup-questions-hidden')
-    }
-)
+if (calculatorButton) {
+    calculatorButton.addEventListener(
+        'click',
+        function () {
+            popupCalculatorHidden.classList.add('popup-calculator-hidden')
+            popupQuestionsHidden.classList.remove('popup-questions-hidden')
+        }
+    )
+}
 
 // Кнопка закрытия окна Вопросы
 var popupQuestionsBtnClose = document.querySelector('.popup-questions-btn-close')
 
-popupQuestionsBtnClose.addEventListener(
-    'click',
-    function () {
-        popupQuestionsHidden.classList.add('popup-questions-hidden')
-        popupContainerHidden.classList.add('popup-container-hidden')
-    }
-)
+if (popupQuestionsBtnClose) {
+    popupQuestionsBtnClose.addEventListener(
+        'click',
+        function () {
+            popupQuestionsHidden.classList.add('popup-questions-hidden')
+            popupContainerHidden.classList.add('popup-container-hidden')
+        }
+    )
+}
 
 // Кнопка перехода на окно завершения
 var questionsLowerBtn = document.querySelector('.questions-lower-btn')
 // Класс скрытия окна Спасибо
 var popupThxHidden = document.querySelector('.popup-thx-hidden')
 
-questionsLowerBtn.addEventListener(
-    'click',
-    function () {
-        popupQuestionsHidden.classList.add('popup-questions-hidden')
-        popupThxHidden.classList.remove('popup-thx-hidden')
-    }
-)
+if (questionsLowerBtn) {
+    questionsLowerBtn.addEventListener(
+        'click',
+        function () {
+            popupQuestionsHidden.classList.add('popup-questions-hidden')
+            popupThxHidden.classList.remove('popup-thx-hidden')
+        }
+    )
+}
 
 // Кнопка Ок окна Спасибо
 var thxBtn = document.querySelector('.thx-btn')
@@ -93,18 +107,22 @@ var thxBtn = document.querySelector('.thx-btn')
 // Кнопка закрытия окна Спасибо
 var popupThxBtnClose = document.querySelector('.popup-thx-btn-close')
 
-popupThxBtnClose.addEventListener(
-    'click',
-    function () {
-        popupThxHidden.classList.add('popup-thx-hidden')
-        popupContainerHidden.classList.add('popup-container-hidden')
-    }
-)
+if (popupThxBtnClose) {
+    popupThxBtnClose.addEventListener(
+        'click',
+        function () {
+            popupThxHidden.classList.add('popup-thx-hidden')
+            popupContainerHidden.classList.add('popup-container-hidden')
+        }
+    )
+}
 
-thxBtn.addEventListener(
-    'click',
-    function () {
-        popupThxHidden.classList.add('popup-thx-hidden')
-        popupContainerHidden.classList.add('popup-container-hidden')
-    }
-)
+if (thxBtn) {
+    thxBtn.addEventListener(
+        'click',
+        function () {
+            popupThxHidden.classList.add('popup-thx-hidden')
+            popupContainerHidden.classList.add('popup-container-hidden')
+        }
+    )
+}
